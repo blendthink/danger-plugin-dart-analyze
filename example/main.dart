63caa3ec3bbb1202fedae7e29227e96fc18e81d3
blendthink <info@blendthink.dev>
2022-07-07 14:49:53 +0900

    initial commit

---
+++ example/main.dart
@@ -0,0 +1,6 @@
+import 'package:danger_plugin_dart_analyze/danger_plugin_dart_analyze.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/danger_plugin_dart_analyze.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library danger_plugin_dart_analyze;
+
+export 'src/danger_plugin_dart_analyze.dart';
+
+// TODO: Export any libraries intended for clients of this package.
